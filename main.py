d8e04146352ae761a197b7f8708026d72a924d89
RTX3070Ti <64041384+RTX3070Ti@users.noreply.github.com>
2021-07-09 13:23:43 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+num1 = float(input("Enter your first number: "))
+op = input("enter your operator: ")
+num2 = float(input("Enter your second number: "))
+num3 = ("You are dumb!")
+if op == ("+"):
+    print(num1 + num2)
+if op == ("-"):
+    print(num1 - num2)
+if op == ("/"):
+    print(num1 / num2)
+if op == ("*"): 
+    print(num1 * num2)
+if (num1 > num2):
+    result= num3
+    print(result)
+
+elif op:
+    print("error!")
+
+
+
+
+
+
+
