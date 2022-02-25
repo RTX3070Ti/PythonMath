e818a9b4a4ea3175dd7dda7c3ea13cf13a6e07a3
RTX3070Ti <64041384+RTX3070Ti@users.noreply.github.com>
2022-02-25 16:56:47 +0100

    Delete main.py

--- Moderate/main.py
+++
@@ -1,9 +0,0 @@
-x=float(input("Enter x component:  "))
-print("You entered", x)
-y=float(input("Enter y component:  "))
-print("You entered", y)
-z=float(input("Enter z component:  "))
-print("You entered", z)
-
-r = (x**2+y**2+z**2)**0.5
-print("The magnitude of vector is", r)
